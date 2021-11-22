51b394b89feff656c73e7cb6f70f5378c99df989
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2021-11-22 09:36:52 -0300

    update script

--- bin/tir-srbench.sh
+++ bin/tir-srbench.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH:$PWD/bin
 #export LD_PRELOAD=/opt/intel/mkl/lib/intel64/libmkl_core.so:/opt/intel/mkl/lib/intel64/libmkl_rt.so:/opt/intel/mkl/lib/intel64/libiomp5.so:/opt/intel/mkl/lib/intel64/libmkl_sequential.so:/opt/intel/mkl/lib/intel64/libmkl_avx2.so
-bin/tir-exe cli $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" "${11}" +RTS -N1
+bin/tir-exe cli $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" "${11}" "${12}" +RTS -N1
