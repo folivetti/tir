7063d7a57a19fb6bdc91b7dc841a83ad4bd13392
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2022-12-30 19:18:57 -0300

    outer fs

--- diversity/runExp.sh
+++ diversity/runExp.sh
@@ -3,5 +3,5 @@
 sed "s/log\/$1/log\/$1_$2$3/; s/= FS/= $2/; s/$1.csv/$1$3.csv/" diversity/$1.cfg > tmp.cfg
 for i in {1..30}
     do
-        stack run config tmp.cfg | grep "Avg. dist.:" | awk -F: '{print $2}' >> diversity/avgdist/$1_$2$3_avgdist.csv
+        stack run config tmp.cfg +RTS -N2 | grep "Avg. dist.:" | awk -F: '{print $2}' >> diversity/avgdist/$1_$2$3_avgdist.csv
     done
