2ba2983f730fd72505b55e554c05c3292fa8974f
folivetti <folivetti@ufabc.edu.br>
2023-06-27 13:29:58 -0300

    telo instructions

---
+++ papers/TELO/scripts/runTIR.py
@@ -0,0 +1,32 @@
+import glob
+import os
+from yaml import load, Loader
+import sys
+
+count = 0
+for fullname in glob.glob(f"pmlb/datasets/{sys.argv[1]}*"):
+    dname = fullname.split("/")[-1]
+    algname = sys.argv.[2]
+    metadata = load(
+                open(f'pmlb/datasets/{dname}/metadata.yaml','r'),
+                Loader=Loader
+        )
+    print(dname)
+    print (metadata['task'])
+    if metadata['task'] != 'regression':
+        continue
+    for seed in [23654, 15795 ,860, 5390, 29802, 21575, 11964, 11284, 22118, 6265]:
+        if os.path.exists(f'srbench/experiment/results_test/{dname}_{algname}_{seed}.json'):
+            continue
+
+        print(f'{dname}_{seed}.job')
+        count=count+1
+
+        fw = open(f'{dname}_{seed}.job', 'w')
+        print(f'''#!/bin/bash
+source activate srbench
+cd srbench/experiment
+python evaluate_model.py ~/pmlb/datasets/{dname}/{dname}.tsv.gz -ml {algname} -seed {seed}
+''', file=fw)
+        fw.close()
+
